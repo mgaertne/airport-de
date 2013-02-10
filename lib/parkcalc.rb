e517cba888187b81d67ea4d09727df1e06a2bde5
Markus Gärtner <markus.gaertner@it-agile.de>
2013-02-10 21:28:21 +0100

    finale Version der ParkCalcPage-Klasse für die Intiialisierungsschritte

--- lib/parkcalc.rb
+++ lib/parkcalc.rb
@@ -1,6 +1,14 @@
 # encoding: utf-8
 class ParkCalcPage
   
+  @@parkplatzIdentifier = 'ParkPlatz'
+  @@optionIdentifier = 'option'
+  @@startPrefix = 'Start'
+  @@abfahrPrefix = 'Abfahr'
+  @@datumTemplate = "%sDatum"
+  @@zeitTemplate = "%sZeit"
+  @@amPMRadioButtonTemplate = "//input[@name='%sZeitampm' and @value='%s']"
+  
   @@ParkdauerMap = {
     '30 Minuten' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '12:30', 'AM']
   }
@@ -13,22 +21,22 @@ class ParkCalcPage
   end
   
   def select(park_platz)
-    selector = @driver.find_element(:name => 'ParkPlatz')
-    selector.find_elements(:tag_name => 'option').find do |option|
+    selector = @driver.find_element(:name => @@parkplatzIdentifier)
+    selector.find_elements(:tag_name => @@optionIdentifier).find do |option|
       option.text == park_platz
     end.click
   end
   
   def enter_park_dauer(dauer)
     startDatum, startZeit, startZeitampm, abfahrDatum, abfahrZeit, abfahrZeitampm = @@ParkdauerMap[dauer]
-    fuelle_Datum_und_Uhrzeit_aus_fuer 'Start', startDatum, startZeit, startZeitampm
-    fuelle_Datum_und_Uhrzeit_aus_fuer 'Abfahr', abfahrDatum, abfahrZeit, abfahrZeitampm
+    fuelle_Datum_und_Uhrzeit_aus_fuer @@startPrefix, startDatum, startZeit, startZeitampm
+    fuelle_Datum_und_Uhrzeit_aus_fuer @@abfahrPrefix, abfahrDatum, abfahrZeit, abfahrZeitampm
   end
   
   def fuelle_Datum_und_Uhrzeit_aus_fuer(formularPrefix, datum, uhrzeit, ampm)
-    enter_into_field :name, "%sDatum" % formularPrefix, datum
-    enter_into_field :name, "%sZeit" % formularPrefix, uhrzeit
-    @driver.find_element(:xpath, "//input[@name='%sZeitampm' and @value='%s']" % [ formularPrefix, ampm ]).click
+    enter_into_field :name, @@datumTemplate % formularPrefix, datum
+    enter_into_field :name, @@zeitTemplate % formularPrefix, uhrzeit
+    @driver.find_element(:xpath, @@amPMRadioButtonTemplate % [ formularPrefix, ampm ]).click
   end
   
   def enter_into_field(wie, was, wert)
